ff2bbc2daa6c2f87f3f5fa2d4c38c898f35e9bca
lovenew <lovenew@lovenew-PC>
2012-11-21 15:55:19 +0800

    1

---
+++ src/com/gmit/gps/EbGPSActivity.java
@@ -0,0 +1,13 @@
+package com.gmit.gps;
+
+import android.os.Bundle;
+import com.mapabc.mapapi.*;
+
+public class EbGPSActivity extends MapActivity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
