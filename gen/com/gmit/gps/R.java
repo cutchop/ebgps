f5c34591668b6c835d1b9011d0775498fdb85ab4
lovenew <lovenew@lovenew-PC>
2012-11-22 17:38:01 +0800

    2

--- gen/com/gmit/gps/R.java
+++ gen/com/gmit/gps/R.java
@@ -11,14 +11,59 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int bg=0x7f020000;
+        public static final int bottom=0x7f020001;
+        public static final int btn_check_off=0x7f020002;
+        public static final int btn_check_off_pressed=0x7f020003;
+        public static final int btn_check_off_selected=0x7f020004;
+        public static final int btn_check_on=0x7f020005;
+        public static final int btn_check_on_pressed=0x7f020006;
+        public static final int btn_check_on_selected=0x7f020007;
+        public static final int button2=0x7f020008;
+        public static final int button2_down=0x7f020009;
+        public static final int button2_over=0x7f02000a;
+        public static final int faceback=0x7f02000b;
+        public static final int ic_launcher=0x7f02000c;
+        public static final int icon2=0x7f02000d;
+        public static final int input_over=0x7f02000e;
+        public static final int login_back=0x7f02000f;
+        public static final int login_head=0x7f020010;
+        public static final int login_input=0x7f020011;
+        public static final int more_select=0x7f020012;
+        public static final int option=0x7f020013;
+        public static final int option_normal=0x7f020014;
+        public static final int option_selected=0x7f020015;
+        public static final int pop=0x7f020016;
+        public static final int qq_btn_check=0x7f020017;
+        public static final int qq_edit_login=0x7f020018;
     }
     public static final class id {
-        public static final int map=0x7f050001;
-        public static final int toggleButton1=0x7f050000;
+        public static final int ImageButton02=0x7f05000a;
+        public static final int LinearLayout01=0x7f050001;
+        public static final int RelativeLayout01=0x7f05000e;
+        public static final int RelativeLayout02=0x7f050002;
+        public static final int TextView01=0x7f050005;
+        public static final int TextView02=0x7f050007;
+        public static final int TextView03=0x7f050009;
+        public static final int btnLockOrNot=0x7f050011;
+        public static final int faceImg=0x7f050003;
+        public static final int loginRoot=0x7f050000;
+        public static final int login_btn_login=0x7f05000c;
+        public static final int login_cb_autologin=0x7f05000d;
+        public static final int login_cb_savepwd=0x7f05000b;
+        public static final int login_edit_account=0x7f050004;
+        public static final int login_edit_pwd=0x7f050006;
+        public static final int login_option=0x7f05000f;
+        public static final int map=0x7f050010;
+        public static final int myspinner=0x7f050008;
+        public static final int poptext=0x7f050012;
+        public static final int user_face_drawable=0x7f050014;
+        public static final int user_faceback_drawable=0x7f050013;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int login=0x7f030000;
+        public static final int main=0x7f030001;
+        public static final int overlay=0x7f030002;
     }
     public static final class string {
         public static final int apikey=0x7f040002;
--- src/com/gmit/gps/EbGPSActivity.java
+++ src/com/gmit/gps/EbGPSActivity.java
@@ -1,13 +1,60 @@
 package com.gmit.gps;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import com.mapabc.mapapi.*;
 
 public class EbGPSActivity extends MapActivity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+
+	MapView map;
+	MapController mapcontrol;
+	Button btnLockOrNot;
+	View popView;
+	TextView popText;
+	private Boolean locked = false;
+	private float lon, lat;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		lon = 114.3704f;
+		lat = 30.5731f;
+		map = (MapView) findViewById(R.id.map);
+		btnLockOrNot = (Button) findViewById(R.id.btnLockOrNot);
+		popView = getLayoutInflater().inflate(R.layout.overlay, null);
+		popText = (TextView) popView.findViewById(R.id.poptext);
+		map.setBuiltInZoomControls(true);
+		mapcontrol = map.getController();
+		mapcontrol.setZoom(14);
+		mapcontrol.setCenter(new GeoPoint((int) (lat * 1E6), (int) (lon * 1E6)));
+		btnLockOrNot.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				if (locked) {
+					locked = false;
+					btnLockOrNot.setText("锁定");
+					popText.setText("未锁定");
+					Toast.makeText(EbGPSActivity.this, "已解锁,现在移动车辆时您将不会收到通知", Toast.LENGTH_LONG).show();
+				} else {
+					locked = true;
+					btnLockOrNot.setText("解锁");
+					popText.setText("已锁定");
+					Toast.makeText(EbGPSActivity.this, "已锁定,车辆被移动时您将收到警示信息", Toast.LENGTH_LONG).show();
+				}
+			}
+		});
+		popText.setText("未锁定");
+
+		map.addView(popView, new MapView.LayoutParams(MapView.LayoutParams.WRAP_CONTENT, MapView.LayoutParams.WRAP_CONTENT, new GeoPoint((int) (lat * 1E6), (int) (lon * 1E6)), MapView.LayoutParams.BOTTOM_CENTER));
+		// reLocation();
+	}
+
+	private void reLocation() {
+		((MapView.LayoutParams) popView.getLayoutParams()).point = new GeoPoint((int) (lat * 1E6), (int) (lon * 1E6));
+	}
 }
